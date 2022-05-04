90877893780e3c7cb48215cae02120cfe2062057
jangidjay <jangid321jay@gmail.com>
2022-05-04 11:07:30 +0530

    room

--- base/views.py
+++ base/views.py
@@ -20,6 +20,9 @@ def room(request, pk):
      context = {'room': room}
      return render(request, 'base/room.html', context) 
 
+def createRoom(request):
+     context = {}
+     return render(request, 'base/room_form.html', context)
 
 #  1.24.00
  
