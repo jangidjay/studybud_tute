f71f206b520bab9b6f64d3a9df27facc9be387c4
jangidjay <jangid321jay@gmail.com>
2022-05-05 11:46:00 +0530

    CRUD

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Room
-
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import Room, Topic
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -11,8 +12,17 @@ from .models import Room
 # ]
 
 def home(request):
-     rooms = Room.objects.all()
-     context = {'rooms': rooms}
+     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+     rooms = Room.objects.filter(
+          Q(topic__name__icontains=q) |
+          Q(name__icontains=q) |
+          Q(description__icontains=q)
+          )
+     topics = Topic.objects.all()
+     room_count = rooms.count()
+
+     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
      return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -21,8 +31,34 @@ def room(request, pk):
      return render(request, 'base/room.html', context) 
 
 def createRoom(request):
-     context = {}
+     form = RoomForm()
+
+     if request.method == 'POST':
+          form = RoomForm(request.POST)
+          if form.is_valid():
+               form.save()
+               return redirect('home')
+          
+     context = {'form': form}
      return render(request, 'base/room_form.html', context)
 
-#  1.24.00
- 
+
+def updateRoom(request, pk):
+     room = Room.objects.get(id=pk)
+     form = RoomForm(instance=room)
+
+     if request.method == 'POST':
+          form = RoomForm(request.POST, instance=room)
+          if form.is_valid():
+               form.save()
+               return redirect('home')
+
+     context = {'form': form}
+     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+     room = Room.objects.get(id=pk)
+     if request.method == 'POST':
+          room.delete()
+          return redirect('home')
+     return render(request, 'base/delete.html', {'obj': room})
